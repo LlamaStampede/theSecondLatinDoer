aca3c6433c1aeb510d483747b60d2e151aea61e3
Michael Lembck <michaellembck@Michaels-MBP.routerlogin.net>
2018-09-17 21:00:42 -0400

    003 - Latin word now highlights when clicked on, and unhighlights when another word is clicked

--- script.js
+++ script.js
@@ -11,26 +11,33 @@ function getWords() {
 getWords()
 
 function highlight(item) {
+    var hightlightedWord = document.getElementsByName("highlighted");
+    if (hightlightedWord.length == 1) {
+        hightlightedWord[0].style.backgroundColor = null;
+        hightlightedWord[0].setAttribute("name", null);
+    }
     document.getElementById("current").innerHTML = item.innerHTML;
     var value = document.getElementById("current").setAttribute("data-value", "on");
+    item.style.backgroundColor = "rgb(81, 225, 252)";
+    item.setAttribute("name", "highlighted");
     
 }
 
 function mouseEnter(item) {
     var value = document.getElementById("current").getAttribute("data-value");
-    item.style.backgroundColor = "rgb(81, 225, 252)";
-    item.style.fontWeight = null; //"bold"
     if (value == "off") {
+        item.style.backgroundColor = "rgb(81, 225, 252)";
+        item.style.fontWeight = null; //"bold"
         document.getElementById("current").setAttribute("data-value", "temp");
         document.getElementById("current").innerHTML = item.innerHTML;
     }
 }
 
 function mouseLeave(item) {
-    item.style.backgroundColor = null;
-    item.style.fontWeight = null;
     var value = document.getElementById("current").getAttribute("data-value");
     if (value == "temp") {
+        item.style.backgroundColor = null;
+        item.style.fontWeight = null;
         document.getElementById("current").setAttribute("data-value", "off");
         document.getElementById("current").innerHTML = "click on or hover over a word";
     }
